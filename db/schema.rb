d6dc8de4e76747a53f71041be6d5e504b8a0127b
Dejan Pantic <dpantic@gmail.com>
2013-11-30 20:46:58 +0100

    ipc_ai added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130315230445) do
+ActiveRecord::Schema.define(version: 20131130192926) do
+
+  create_table "ipc_airbuses", force: true do |t|
+    t.string   "ac_type"
+    t.string   "ata"
+    t.string   "system"
+    t.string   "description"
+    t.string   "fin"
+    t.string   "add_infos"
+    t.string   "part_number"
+    t.string   "add_material_info"
+    t.string   "ipc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microposts", force: true do |t|
     t.string   "content"
