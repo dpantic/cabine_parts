a4f1689cf710524183452a24191e58b79f86c97d
Dejan Pantic <dpantic@gmail.com>
2014-02-23 11:28:46 +0100

    database change

--- db/migrate/20131130192926_create_ipc_airbuses.rb
+++ db/migrate/20131130192926_create_ipc_airbuses.rb
@@ -3,10 +3,10 @@ class CreateIpcAirbuses < ActiveRecord::Migration
     create_table :ipc_airbuses do |t|
       t.column :ac_type, :string, :limit => 6
       t.column :ata, :string, :limit => 5
-      t.column :system, :string, :limit => 15
+      t.column :system, :string
       t.column :description, :string
       t.column :fin, :string
-      t.column :add_infos, :string, :limit => 20
+      t.column :add_infos, :string, :limit => 15
       t.column :part_number, :string
       t.column :add_material_info, :limit => 15
       t.column :ipc, :string
